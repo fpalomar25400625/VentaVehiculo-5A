65dd36f18f443df64ff2021442ec630713060d84
Josue Palomar <fernandopalomar46@gmail.com>
2026-02-13 19:50:00 -0700

    Feat: Clase Vehiculo completa

--- Proyecto-VentaVehiculo-5A/src/Cliente.java
+++ Proyecto-VentaVehiculo-5A/src/Cliente.java
@@ -9,11 +9,34 @@ public class Cliente {
         this.email = email;
     }
     
+    public String getRS() {
+        return this.rs;
+    }
+    
+    public String getRFC() {
+        return this.rfc;
+    }
+    
+    public String getEmail() {
+        return this.email;
+    }
+    
+    public void setRS(String rs) {
+        this.rs = rs;
+    }
+    
+    public void setRFC(String rfc) {
+        this.rfc = rfc;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     @Override
     public String toString() {
         return "Razon Social " + this.rs + "\n" + 
                "RFC " + this.rfc + "\n" + 
                "Email " + this.email  + "\n";
-    }
-    
+    }   
 }
